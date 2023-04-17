bdc1490b0d885456882b881324004707ed3821fb
ChehaYuri <sacra1364069@gmail.com>
2023-04-17 22:06:08 +0300

    Task 3

--- main3.py
+++ main3.py
@@ -1,26 +1,25 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 
-data = np.loadtxt('file.csv', delimiter=',')
-matrix = np.array(data)
-print(matrix)
-# # построение графика
-# plt.plot(data)
-#
-# # название графика и обозначение осей
-# plt.title('Название графика')
-# plt.xlabel('Ось X')
-# plt.ylabel('Ось Y')
-#
-# # рассчет среднеквадратичного отклонения
-# stddev = np.std(data)
-#
-# # вывод значения среднеквадратичного отклонения на график
-# plt.text(0.5, 0.9, f'СКО = {stddev:.2f}', ha='center', va='center', transform=plt.gca().transAxes)
-#
-# # сохранение графика в формате jpeg
-# plt.savefig('plot.jpg')
-#
-# # отображение графика
-# plt.show()
+data = pd.read_excel(r'C:\Users\user\Downloads\dataMatplotlib.xlsx')
+
+# построение графика
+plt.plot(data)
+
+# название графика и обозначение осей
+plt.title('Название графика')
+plt.xlabel('Ось X')
+plt.ylabel('Ось Y')
+
+# рассчет среднеквадратичного отклонения
+stddev = np.std(data)
+
+# вывод значения среднеквадратичного отклонения на график
+
+# сохранение графика в формате jpeg
+plt.savefig('plot.jpg')
+
+# отображение графика
+plt.show()
